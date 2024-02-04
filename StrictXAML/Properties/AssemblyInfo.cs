adcac34c3d356e96b5aeb2fa2f3c9d4b9bfb309e
Michael L Perry <michael@qedcode.com>
2024-02-04 12:12:50 -0600

    Move package properties into csproj

--- StrictXAML/Properties/AssemblyInfo.cs
+++ StrictXAML/Properties/AssemblyInfo.cs
@@ -7,12 +7,6 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StrictXAML")]
-[assembly: AssemblyDescription("XAML controls that follow strict guidelines to fix common issues.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Michael L Perry")]
-[assembly: AssemblyProduct("StrictXAML")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +45,3 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
